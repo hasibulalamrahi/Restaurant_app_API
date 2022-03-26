79c7d7753f4bd82ad56880f65d0a1c5f0bdc2c19
asad <asad.hnk@gmail.com>
2022-03-26 13:44:22 +0600

    get post route modifications

--- routeHandler/allItems/allItems.js
+++ routeHandler/allItems/allItems.js
@@ -5,13 +5,36 @@ const allItemSchema = require('../../schemas/allItemSchema')
 const AllItem = new mongoose.model('AllItem', allItemSchema)
 
 //Get All the Items
-router.get('/',async(req,res)=>{
-
+router.get('/',(req,res)=>{
+    AllItem.find({isStatus: 'active'})
+        .exec()
+        .then(docs =>{
+            console.log(docs)
+            res.status(200).json(docs)
+        })
+        .catch(err=>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 //Get A Particular Item
-router.get('/:id',async(req,res)=>{
-    
+router.get('/:id',(req,res)=>{
+    const id = req.params.id
+    AllItem.findById(id)
+        .exec()
+        .then(docs =>{
+            console.log(docs)
+            res.status(200).json(docs)
+        })
+        .catch(err=>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 //Post An Item
@@ -32,17 +55,53 @@ router.post("/",async(req,res)=>{
 
 //Post Multiple Item
 router.post('/all',async(req,res)=>{
-    
+    await AllItem.insertMany(req.body,(err)=>{
+        if(err){
+            res.status(500).json({
+                error:"There was Server Side error"
+            })
+        }else{
+            res.status(200).json({
+                message:"All the Items were inserted Successfully"
+            })
+        }
+    })
 })
 
 //Put An Item
 router.put('/:id',async(req,res)=>{
-    
+    await AllItem.updateOne({_id:req.params.id},{
+        $set:{
+            isStatus: 'active',
+            
+        }
+    },(err)=>{
+        if(err){
+            res.status(500).json({
+                error:"There was Server Side error"
+            })            
+        } else {
+            res.status(200).json({
+                message:"Item was edited Successfully"
+            })
+        }
+    })
 })
 
 //Delete An Item
-router.delete('/:id',async(req,res)=>{
-    
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id
+    AllItem.deleteOne({id: id})
+        .exec()
+        .then(result =>{
+            res.status(200).json(result)
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router 
